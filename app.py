3e4f7b9892f5c4f358db139e55f7d1ea0f8f1697
Luigi Marino <emails@luigi-marino.com>
2023-06-08 15:13:57 +0100

    Documentatation of code

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 
+# =================================================================
+# Description
+# # This is the entry point of the application and routing management.
+# =================================================================
+
+# =================================================================
+# Imports                                                         #
+# =================================================================
+# # sqlite3 - Implements a small, fast, self-contained, SQL database engine
+# # Flask - Flask is a micro web framework written in Python
+# # werkzeug - A comprehensive WSGI web application library.
 import sqlite3
 from flask import Flask, render_template
 from werkzeug.exceptions import abort
 
+
+# =================================================================
+# Functions                                                       #
+# =================================================================
+# get_db_connection()
+# # Connects to the database and returns a var containing the database rows.
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+# get_posts()
+# # Uses the database connection to select all posts where id = post_id
+# # Selects the first post and returns that object.
+# # If none returned abort with a 404 error.
 def get_posts(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
@@ -16,8 +37,19 @@ def get_posts(post_id):
         abort(404)
     return post
 
+# =================================================================
+# Application Specific                                            #
+# =================================================================
+# # Assign Flask(__name__) to app for better readability
 app = Flask(__name__)
 
+
+# =================================================================
+# Flask Routes                                                    #
+# =================================================================
+# # Index: Home Page
+# # Loading the page creates a connection to the database, fetches all posts
+# # and then loads each into the index.html template.
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -25,6 +57,9 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+# # Blog: Page specifically for blog posts
+# # Currently just a holding item for the menu.
+# # Returns a simple string 'Blog Page'.
 @app.route('/blog/')
 def blog():
     return 'Blog Page'
--- init_db.py
+++ init_db.py
@@ -1,15 +1,31 @@
+# =================================================================
+# Description
+# # The purpose of this module is to initilize the database and add two posts.
+# =================================================================
+
+# =================================================================
+# Imports                                                         #
+# =================================================================
+# # sqlite3 - Implements a small, fast, self-contained, SQL database engine
 import sqlite3
 
+# =================================================================
+# Application Specific                                            #
+# =================================================================
+# Create a variable for the database connection.
 connection = sqlite3.connect('database.db')
-
+# Open the database connection.
 with open('schema.sql') as f:
     connection.executescript(f.read())
-
+# Create a variable for the connection cursor.
+# This is used to navigate through the database using executed commands.
 cur = connection.cursor()
 
+# Use the cursor object to run the command "INSERT INTO posts (title, content) VALUES (?, ?)"
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)", ('First Post','Content for first post'))
-
+# Use the cursor object to run the command "INSERT INTO posts (title, content) VALUES (?, ?)"
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)", ('Second Post','Content for second post'))
-
+# Use the cursor object to commit these changes.
 connection.commit()
+# Use the cursor object to close the database connection.
 connection.close()
